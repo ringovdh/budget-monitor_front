e190bc7cc718412af6b22ecd47b3a7480de9602d
Van den Hende Ringo <>
2023-02-05 12:30:51 +0100

    add graphs

--- src/app/budget/graphs/graphs.component.ts
+++ src/app/budget/graphs/graphs.component.ts
@@ -1,4 +1,6 @@
-import {Component, OnChanges, SimpleChanges} from '@angular/core';
+import {Component, Input, OnChanges, SimpleChanges} from '@angular/core';
+import { Chart } from 'chart.js/auto';
+import { BudgetOverviewPerCategory } from 'src/app/entity/BudgetOverviewPerCategory';
 
 @Component({
   selector: 'app-graphs',
@@ -8,36 +10,84 @@ import {Component, OnChanges, SimpleChanges} from '@angular/core';
 })
 export class GraphsComponent implements OnChanges {
 
-  pieChart: Chart;
-  constructor() { }
+  fixedOutgoingBudgetPieChart: any;
+  outgoingBudgetPieChart: any;
+  @Input() fixedOutgoingBudget: BudgetOverviewPerCategory[] = [];
+  @Input() outgoingBudget: BudgetOverviewPerCategory[] = [];
+
+  constructor() {
+  }
 
   ngOnChanges(changes: SimpleChanges): void {
-    if (this.pieChart != null) {
-      this.pieChart.destroy();
-    }
+    this.createFixedOutgoingBudgetGraph();
+    this.createOutgoingBudgetGraph();
+  }
 
-this.pieChart = new Chart('pieChartMonths', {
-  type: 'pie', data: {
-    labels: periods,
-    datasets: [
-      { data: amounts,
-        borderColor: '#303e45',
-        backgroundColor: backgroundColors,
+  private createOutgoingBudgetGraph() {
+    if (this.outgoingBudget != null) {
+      let categories: string[] = [];
+      let amounts: number[] = [];
+      let backgroundColors: string[] = ['#BFD7ED', '#60A3D9', '#0074B7', '#003B73', '#0000FF', '#4B7BF5', '#79A9F5', '#C2E2F5'];
+      this.outgoingBudget.forEach(value => {
+        categories.push(value.category.label);
+        amounts.push(value.total);
+      });
+
+      if (this.outgoingBudgetPieChart != null) {
+        this.outgoingBudgetPieChart.destroy();
       }
-    ]
-  },
-  options: {
-    legend: {
-      display: true
-    },
-    scales: {
-      xAxes: [{
-        display: false
-      }],
-      yAxes: [{
-        display: false
-      }],
+
+      this.outgoingBudgetPieChart = new Chart('pieChartOutgoingBudget', {
+        type: 'pie',
+        data: {
+          labels: categories,
+          datasets: [
+            {
+              data: amounts,
+              borderColor: '#303e45',
+              backgroundColor: backgroundColors,
+              hoverOffset: 4
+            }
+          ]
+        },
+        options: {
+          responsive: true
+        }
+      });
     }
   }
-});
+
+  private createFixedOutgoingBudgetGraph() {
+    if (this.fixedOutgoingBudget != null) {
+      let categories: string[] = [];
+      let amounts: number[] = [];
+      let backgroundColors: string[] = ['#05445E', '#189AB4', '#75E6DA', '#D4F1F4', '#145DA0', '#0C2D48', '#2E8BC0', '#B1D4E0'];
+      this.fixedOutgoingBudget.forEach(value => {
+        categories.push(value.category.label);
+        amounts.push(value.total);
+      });
+
+      if (this.fixedOutgoingBudgetPieChart != null) {
+        this.fixedOutgoingBudgetPieChart.destroy();
+      }
+
+      this.fixedOutgoingBudgetPieChart = new Chart('pieChartFixedOutgoingBudget', {
+        type: 'pie',
+        data: {
+          labels: categories,
+          datasets: [
+            {
+              data: amounts,
+              borderColor: '#303e45',
+              backgroundColor: backgroundColors,
+              hoverOffset: 4
+            }
+          ]
+        },
+        options: {
+          responsive: true
+        }
+      });
+    }
+  }
 }
