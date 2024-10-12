40c753759c48b75cb5440a54341a47d2db1347f3
Van den Hende Ringo <>
2024-10-12 12:33:45 +0200

    remove unused component

--- src/app/admin/transaction/transaction-routing.module.ts
+++ src/app/admin/transaction/transaction-routing.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { IndexComponent } from "./index/index.component";
 
 const routes: Routes = [
   { path: 'transaction', redirectTo:'transaction/index', pathMatch: 'full' },
-  { path: 'transaction/index', component: IndexComponent },
 ];
 
 @NgModule({
--- src/app/admin/transaction/transaction.module.ts
+++ src/app/admin/transaction/transaction.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { TransactionRoutingModule } from './transaction-routing.module';
-import { IndexComponent } from "./index/index.component";
 import { CreateComponent } from "./create/create.component";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { NgxPaginationModule } from "ngx-pagination";
@@ -11,7 +10,6 @@ import { EditComponent } from './edit/edit.component';
 
 @NgModule({
   declarations: [
-    IndexComponent,
     CreateComponent,
     EditComponent
   ],
